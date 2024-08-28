4739496debae7282b92e454a699b866ce05dc29a
Venki <venki@Venkis-MacBook-Pro.local>
2024-08-28 13:42:10 +0530

    Demo project 2 completed

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return(
+        <div className="card">
+            <img className="card-image" src="https://media.licdn.com/dms/image/v2/C5603AQHt1wJz1afuUQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1645273561199?e=1730332800&v=beta&t=jUObOtPUP7VyUmrN4VbgFzt1ZtrlyqAICTIr5QK3tzs" alt="Profile Picture" />
+            <h2 className="card-title">Venki B</h2>
+            <p className="card-text">I am a Full Stack Developer</p>
+        </div>
+    );
+}
+
+export default Card
